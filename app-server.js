019943278dad153b3954872d66553f37359532d6
Froilan Miranda <froilan.miranda@gmail.com>
2016-05-24 21:27:00 -0400

    add socket.io on server and client

--- app-server.js
+++ app-server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+var _ = require('underscore');
+
+var connections = [];
+var users = [];
 
 app.use(express.static('./public'));
 app.use(express.static('./node_modules/bootstrap/dist'));
@@ -7,4 +11,34 @@ app.use(express.static('./node_modules/bootstrap/dist'));
 var server = app.listen(3000);
 var io = require('socket.io').listen(server);
 
-console.log("Polling server is running at 'http://localhost:3000'");
+io.sockets.on('connection', function(socket){
+	socket.once('disconnect', function(){
+		var member = _.findWhere(users, { id: this.id });
+
+		if(member){
+			users.splice(users.indexOf(member), 1);
+			io.sockets.emit('users', users);
+			console.log("Left: %s (%s audience members)", member.name, audience.length);
+		}
+
+		connections.splice(connections.indexOf(socket), 1);
+	    socket.disconnect();
+	    console.log("Disconnected: %s sockets remaining", connections.length)
+	});
+
+	socket.on('join', function(payload){
+		var newMember = {
+			id: this.id,
+			name: payload.name,
+		}
+		this.emit('joined', newMember);
+		users.push(newMember);
+		io.sockets.emit('users', users);
+		console.log("User Joined: %s", payload.name);
+	});
+
+	connections.push(socket);
+	console.log("Connected: %s sockets remaining", connections.length);
+});
+
+console.log("Calculator server is running at 'http://localhost:3000'");
--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,33 @@
 import Display from './Display';
 import Header from './Header';
+import IO from 'socket.io-client';
 import Join from './Join';
 import React from 'react';
 
 class App extends React.Component {
   constructor() {
     super();
+    this.emit = this.emit.bind(this);
     this.state = {
       status: 'disconnected',
       member: {name:null}
     }
   }
+
+  componentWillMount(){
+    this.socket = IO('http://localhost:3000');
+  }
+
+  emit(eventName, payload){
+    this.socket.emit(eventName, payload);
+  }
+
   render() {
     return (
       <div>
         <Header {...this.state}/>
         <Display if={!this.state.member.name}>
-          <Join emit={this.props.emit}/>
+          <Join emit={this.emit}/>
         </Display>
       </div>
     )
